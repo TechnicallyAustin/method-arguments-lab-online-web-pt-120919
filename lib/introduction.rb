865df977d2b37ac17ff523f1c96975f0f08794ab
Austin Johnson <technicallyaustin@gmail.com>
2019-12-10 19:36:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ puts "Hi, my name is Austin"
 end
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
